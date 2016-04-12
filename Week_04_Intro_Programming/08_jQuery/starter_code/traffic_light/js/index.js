23be88ae27d886c5467d7e1a884d52d4d8178218
singida <krissykimura@gmail.com>
2016-04-12 18:47:22 -0400

    updates

--- Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,41 @@
+$(document).ready(function() {
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+
+var stopButtonId = '#stopButton';
+
+
+$('#stopButton').click(colorStopLight);
+$('#slowButton').click(colorSlowLight);
+$('#goButton').click(colorGoLight);
+
+function colorStopLight() {
+	$('#stopLight').css('backgroundColor', 'red');
+	$('#slowLight, #goLight').css('backgroundColor', 'black');
+}
+
+function colorSlowLight() {
+	$('#stopLight, #goLight').css('backgroundColor', 'black');
+	$('#slowLight').css('backgroundColor', 'yellow');
+}
+
+function colorGoLight() {
+	$('#stopLight, #slowLight').css('backgroundColor', 'black');
+	$('#goLight').css('backgroundColor', 'green');
+}
+
+colorStopLight();
+
+setInterval(function() {
+	colorStopLight();
+
+	setTimeout(function() {
+	colorGoLight();
+
+	setTimeout(function() {
+	colorSlowLight();
+	}, 2000);
+}, 3000);
+}, 6000);
+
+});
